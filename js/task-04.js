a22ab65319b9d20afd1c67be2de1bf3599c4a180
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-04 21:20:14 +0300

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,37 +1,48 @@
-class CounterPlag {
-  counterValue = 0;
-  counterStep = 1;
+// class CounterPlag {
+//   counterValue = 0;
 
-  constructor({ value, step } = {}) {
-    this._counterValue = value;
-    this._counterStep = step;
-    this._refs = this._getRefs;
-  }
+//   constructor({ value, step } = {}) {
+//     this.counterValue = value;
+//     this.counterStep = step;
+//   }
 
-  _getRefs() {
-    const refs = {};
-    refs.decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
-    refs.ncrementBtn = document.querySelector('#counter button[data-action="increment"]');
-    refs.valueBtn = document.querySelector('#value"]');
-    return refs;
-  }
+//   getRefs() {
+//     const refs = {};
+//     refs.decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
+//     refs.ncrementBtn = document.querySelector('#counter button[data-action="increment"]');
+//     refs.valueBtn = document.querySelector('#value"]');
+//     return refs;
+//   }
 
-  performActions() {
-    decrementBtn.addEventListener('click', () => {
-      this.decrement();
-      console.log('decrement');
-    });
+//   performActions() {
+//     this.refs.decrementBtn.addEventListener('click', () => {
+//       this.decrement();
+//       console.log('decrement');
+//     });
 
-    incrementBtn.addEventListener('click', () => {
-      this.increment();
-      console.log('increment');
-    });
-  }
-  increment(step) {
-    this.counterStep += step;
-  }
-  decrement(step) {
-    this.counterValue -= step;
-  }
-}
-const counter = new CounterPlag({ counterStep: 1 });
+//     incrementBtn.addEventListener('click', () => {
+//       this.increment();
+//       console.log('increment');
+//     });
+//   }
+//   increment(step) {
+//     this.counterStep += step;y
+//   }
+//   decrement(step) {
+//     this.counterValue -= step;
+//   }
+// }
+// const counter = new CounterPlag({ counterStep: 1 });
+
+const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
+const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
+const valueBtn = document.querySelector('#value');
+let counterValue = 0;
+decrementBtn.addEventListener('click', () => {
+  counterValue += 1;
+});
+
+incrementBtn.addEventListener('click', () => {
+  console.log('increment');
+});
+console.log(counterValue);
