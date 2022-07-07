615f1e4b138c15e2ba0de5628d7ade4f09b21162
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-07 21:04:55 +0300

    taska - 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,10 +2,8 @@ const refs = {
   textInput: document.querySelector('#font-size-control'),
   textOutput: document.querySelector('#text'),
 };
-setAttribute(
-  'style',
-  'list-style-type: none; display: flex; align-items: center; justify-content: space-evenly',
-);
+
+refs.textInput.value = refs.textInput.min;
 refs.textInput.addEventListener('input', event => {
-  refs.textOutput.textContent = event.currentTarget.value;
+  refs.textOutput.style.fontSize = event.currentTarget.value + 'px';
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const formOutput = ({
+    elements: { email, password },
+  } = event.currentTarget);
+
+  if (formOutput.elements.email.value === '' || formOutput.elements.password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+}
