e0f269b8f3e8177bad7fa52b44e84fc41ad8c913
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-07-11 10:03:04 +0300

    task - 10 fix bag

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,6 @@
-const inputField = document.querySelector('input');
 const refBoxes = document.querySelector('#boxes');
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
-
-let createGlobalNumber = [];
 
 function getAmount() {
   var amount = +document.querySelector('#controls input').value;
@@ -11,22 +8,17 @@ function getAmount() {
 }
 
 function createBoxes(amount) {
-  let createLocalNumber = [];
-  createGlobalNumber = createLocalNumber;
-
+  let localBox = [];
   let sumSizesDiv = 30;
   for (let i = 0; i < amount; i += 1) {
-    createLocalNumber.push(
+    localBox.push(
       `<div class = "boxes__div" style=" background-color : ${getRandomHexColor()}; 
       height : ${sumSizesDiv + 10}px; 
       width : ${sumSizesDiv + 10}px"> ${i + 1} </div>`,
     );
     sumSizesDiv += 10;
   }
-}
-
-function createBoxes() {
-  refBoxes.insertAdjacentHTML('afterbegin', createGlobalNumber.join(''));
+  refBoxes.insertAdjacentHTML('afterbegin', localBox.join(''));
 }
 
 function destroyBoxes() {
@@ -38,6 +30,5 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-inputField.addEventListener('input', getAmount);
-btnCreate.addEventListener('click', createBoxes);
+btnCreate.addEventListener('click', getAmount);
 btnDestroy.addEventListener('click', destroyBoxes);
